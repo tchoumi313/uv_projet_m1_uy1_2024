38bbf7e5ab94feeb8366d88607c736cde43dfa9a
tchoumi313 <tchouminzikeubd@gmail.com>
2024-06-17 22:34:28 +0000

    update app.py port binding

--- api/app.py
+++ api/app.py
@@ -233,6 +233,6 @@ def upload_file():
         }
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 3000))  # Get the PORT environment variable or default to 5000
+    port = int(os.environ.get("PORT", 5000))  # Get the PORT environment variable or default to 5000
     app.run(port=port)
 
