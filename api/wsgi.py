23a364aa932c3b24eb14da21908d0e79a4a73700
tchoumi313 <tchouminzikeubd@gmail.com>
2024-06-17 22:59:40 +0000

    adding wsgi.py

--- api/wsgi.py
+++ api/wsgi.py
@@ -1,4 +1,5 @@
 from app import app
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))  # Get the PORT environment variable or default to 5000
+    app.run(port=port)
